212ec7ed9fd3f8c8df05503a67aaaa82814de7ad
Vincenzo Ampolo <vincenzo.ampolo@gmail.com>
2011-06-15 17:01:54 +0200

    discarded added

--- keygrabber/i_can_do_better_than_parsit.py
+++ keygrabber/i_can_do_better_than_parsit.py
@@ -3,7 +3,7 @@ import pymongo
 connection = pymongo.Connection()
 db = connection.webkeywords
 
-print "keyword, index"
+print "keyword, index, discarded"
 
 for item in db.orderedkeys.find():
 	keyword = item.get('keyword')
@@ -12,3 +12,5 @@ for item in db.orderedkeys.find():
 		word = words[1]
 		if word.endswith('are') or word.endswith('ere') or word.endswith('ire'):
 			print "%s, %s" % (item.get('keyword'), item.get('index')) 
+		else:
+			print "%s, %s, True" % (item.get('keyword'), item.get('index')) 
