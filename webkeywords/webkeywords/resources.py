d6ef7371a23a292df0b50145a5a66ed48fd44dc1
Vincenzo Ampolo <vincenzo.ampolo@gmail.com>
2011-08-24 13:06:25 +0200

    show global and regional in web

--- webkeywords/webkeywords/resources.py
+++ webkeywords/webkeywords/resources.py
@@ -69,30 +69,30 @@ class Category(object):
 			i.__parent__= self 
 			return i
 
-		
-        
 class InstantKeywordMongo(object):
- __parent__= None 
- __name__ = None
-	
- def __init__(self, keyword=None, parent=None, category=None, level=None, dicts=None, depth=None, place=None, **kwargs):
-  self.keyword = keyword
-  self.parent = parent
-  self.category = category
-  self.level = level
-  self.dicts = dicts
-  self.depth = depth
-  self.place = place
-  self.has_child = False
-  self.fields = ['keyword', 'level', 'dicts', 'depth', 'place', 'category', 'parent', 'has_child']
- 
- def __str__(self):
-  return '%s' % (self.keyword)
- 
- def to_dict(self):
-  d = dict()
-  for field in self.fields:
-  	value = getattr(self, field)
-  	if value is not None and value is not False:
-  		d[field] = value
-  return d
+	__parent__= None 
+	__name__ = None
+    
+    def __init__(self, keyword=None, parent=None, category=None, level=None, dicts=None, depth=None, place=None, dbplace=None, **kwargs):
+        self.keyword = keyword
+        self.parent = parent
+        self.category = category
+        self.level = level
+        self.dicts = dicts
+        self.depth = depth
+        self.place = place
+        self.dbplace = dbplace
+        self.has_child = False
+        self._id = None
+        self.global_searches = None
+        self.regional_searches = None
+        self.fields = ['keyword', 'level', 'dicts', 'depth', 'place', 'dbplace', 'category', 'parent', 'has_child', '_id', 'global_searches', 'regional_searches']
+    def __str__(self):
+        return 'keyword: %s, parent: %s, dicts: %s, level: %s, depth: %s, dbplace: %s, place: %s' % (self.keyword, self.parent, self.dicts, self.level, self.depth, self.dbplace, self.place)
+    
+    def to_dict(self):
+        d = dict()
+        for field in self.fields:
+        	if getattr(self, field) is not None:
+        		d[field] = getattr(self, field)
+        return d
