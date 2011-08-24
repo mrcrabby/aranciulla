8f3bba603cb0cb560d85c5d6d5c8165757f12bf8
Vincenzo Ampolo <vincenzo.ampolo@gmail.com>
2011-08-24 12:21:43 +0200

    new algorithm

--- keygrabber/ordering.py
+++ keygrabber/ordering.py
@@ -13,6 +13,9 @@ CL_IN = DB.crawlerCopy
 CL_OUT = DB.orderedkeys
 
 def create_ordered_collection(inst_list):
+	'''
+	Create the CL_OUT collection adding an index element
+	'''
 		#add index
 		log.info('adding index')
 		for i, key in enumerate(inst_list):
@@ -25,7 +28,20 @@ def create_ordered_collection(inst_list):
 		log.info('collection created successfully')
 		return inst_list
 
-def adwords_ordering(self, limit = 0):	
+def get_adwords_data(self, limit = 0):	
+	'''
+	Get adwords data and save it
+	'''
+	for item in CL_IN.find().limit(limit):
+		d = kad.get_keyword_info(item.get('keyword'))
+		item['global_searches'] = d.get('global_searches')
+		item['regional_searches'] = d.get('regional_searches')
+		CL_IN.save(item)
+	
+def order_by_adwords_data(keys):
+	keys.sort(key=lambda x: x['global_searches'] if 'global_searches' in x and x['global_searches'] is not None else 0 , reverse=True)
+
+def adwords_ordering(limit = 0):
 	'''
 	Il nuovo algoritmo di
 	ordinamento funziona così: si fa query a Google per ogni keyword per
@@ -37,11 +53,13 @@ def adwords_ordering(self, limit = 0):
 	come scaricare musica fa 100 ricerche al mese e come scaricare ne fa
 	10000)
 	'''
+	keys = list()
+	#get_adwords_data(limit)
 	for item in CL_IN.find().limit(limit):
-		d = kad.get_keyword_info(item.get('keyword'))
-		item['global_searches'] = d.get('global_searches')
-		item['regional_searches'] = d.get('regional_searches')
-		CL_IN.save(item)
+		keys.append(item)
+	ordered_keys = order_by_adwords_data(keys)
+	create_ordered_collection(ordered_keys)
+		
 
 if __name__ == "__main__":
-	adwords_ordering(200000)
+	adwords_ordering(20000)
